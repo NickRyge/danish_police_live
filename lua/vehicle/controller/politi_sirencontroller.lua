4f7f6eb659cd2f8ae08d6a71fd8e303cf458d0cc
Niclas <nikkeryge@live.dk>
2024-08-11 23:33:49 +0200

    Switched sirencontroller to rely on electricsvals - Hopefully improves multiplayer readiness

--- lua/vehicle/controller/politi_sirencontroller.lua
+++ lua/vehicle/controller/politi_sirencontroller.lua
@@ -2,27 +2,32 @@
 
 local M = {}
 
-
 local createdSounds = false
 
 local currentSiren
 local sirenArray = {}
+local prevHornStatus = 0
 local currentSirenCount = 0
 
 
-
-
+---Function that plays a specified soundscape sound.
+---@param siren v.data.soundscape Siren from the soundscape table to play.
 local function playSiren(siren)
     currentSiren = siren
-    print(currentSiren)
     obj:setVolume(currentSiren, 1)
     obj:playSFX(currentSiren)
+    -- print(currentSiren)
 end
 
-local function stopSiren(siren) 
+
+---Funciton that stops a specified soundscape sound.
+---@param siren v.data.soundscape Siren from the soundscape table to stop.
+local function stopSiren(siren)
     obj:cutSFX(siren)
 end
 
+
+---Called to reset the sirenController back to 0.
 local function reset()
     currentSirenCount = 0
 
@@ -32,10 +37,11 @@ local function reset()
 end
 
 
-function UpdateSirenSound(val)
+---Updates which siren currently plays. Skips the val amount of sirens ahead.
+---@param val number The number of sirens to skip. Default is 1, to play the next siren.
+local function UpdateSirenSound(val)
 
-
-    if (currentSiren ~= nil) then 
+    if (currentSiren ~= nil) then
         stopSiren(currentSiren)
     end
 
@@ -47,21 +53,24 @@ function UpdateSirenSound(val)
     else
         playSiren(sirenArray[currentSirenCount])
     end
-
 end
 
 
-local function startup() 
-    for key, value in pairs(v.data.soundscape) do
-        if (string.find(key, "siren")) then
-            table.insert(sirenArray, sounds.createSoundscapeSound(key))
+---Initializes the controller by creating the soundscapesounds for each siren found.
+---Dynamically discovers all sirens a car has and readies them.
+local function startup()
+    for soundName, _ in pairs(v.data.soundscape) do
+        if (string.find(soundName, "siren")) then
+            table.insert(sirenArray, sounds.createSoundscapeSound(soundName))
         end
     end
 end
 
 
+---The beamng-specific updateGFX function, that runs once per graphics tick.
+---@param dt number DeltaTime - see https://en.wikipedia.org/wiki/Delta_timing or the beamng docs.
 local function updateGFX(dt)
-    -- for some reason this must be done once and for all, and doesn't work in INIT. >:|
+    -- Must be done here, init loads too quickly to work properly.
     if not createdSounds then
         startup()
         createdSounds = true
@@ -69,15 +78,21 @@ local function updateGFX(dt)
 
     if currentSiren ~= nil and electrics.values["lightbar"] < 1 then
         reset()
+        return
     end
+
+    if electrics.values.horn == 1 and prevHornStatus ~= 1 then
+        UpdateSirenSound(1)
+    end
+
+    prevHornStatus = electrics.values.horn
 end
 
 
-
+---Function to init the controller.
 local function init()
-
+    --Placeholder, doesn't do anything right now.
 end
-
 
 
 M.init = init
